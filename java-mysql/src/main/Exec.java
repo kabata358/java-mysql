d3031e634863bfcc5324dbc86f5023a22b784082
kabata358 <kabata358@gmail.com>
2022-07-05 20:28:18 +0900

    Completed connection to database

--- java-mysql/src/main/Exec.java
+++ java-mysql/src/main/Exec.java
@@ -1,10 +1,45 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Exec {
 
 	public static void main(String[] args) {
 		
-		
+		try {
+			
+			// データベースに接続
+			Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost/sample?useSSL=false",
+				"",
+				""
+			);
+			
+			// SQL文の実行
+			PreparedStatement pstmt = con.prepareStatement("SELECT * FROM member");
+			ResultSet rs = pstmt.executeQuery();
+			
+			// 検索結果を表示
+			while (rs.next()) {
+				System.out.println(rs.getString("name"));
+				System.out.println(rs.getString("sex"));
+			}
+			
+			System.out.println("Connected");
+			
+			// 後処理（リソースのクローズ）
+			rs.close();
+			pstmt.close();
+			con.close();
+			
+		} catch ( Exception e) {
+			
+			System.out.println(e);
+			
+		}
 		
 	}
 
