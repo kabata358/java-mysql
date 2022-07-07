7befbb30b43cf10964ac57cf3b0c2ad5bb33bba5
kabata358 <kabata358@gmail.com>
2022-07-07 22:37:02 +0900

    Implemented information addition function

--- java-mysql/src/main/Exec.java
+++ java-mysql/src/main/Exec.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
 
 public class Exec {
 
@@ -23,22 +26,65 @@ public class Exec {
 			ResultSet rs = pstmt.executeQuery();
 			
 			// 検索結果を表示
+			/*
 			while (rs.next()) {
 				System.out.println(rs.getString("name"));
 				System.out.println(rs.getString("sex"));
 			}
+			*/
 			
 			System.out.println("Connected");
 			
 			
-			String sql = "INSERT INTO member (name, sex, old, enter) VALUES ('大宮太一', '男', '32', '2022-06-27');";
-			pstmt.executeUpdate(sql);
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.print("[情報追加：1　終了：0]　->　");
+			
+			while(scanner.nextInt() == 1) {
+				
+				String name, sex;
+				int old;
+				
+				// 情報入力
+				System.out.print("名前：");
+				name = scanner.next();
+				System.out.print("性別：");
+				sex = scanner.next();
+				System.out.print("年齢：");
+				old = scanner.nextInt();
+
+				
+				// 日付を取得
+				Calendar cl = Calendar.getInstance();
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				String today = sdf.format(cl.getTime());
+				
+				
+				System.out.println(name + ", " + sex + ", " + old);
+				System.out.print("データを追加しますか？　[追加：1　取り消し：0]　->　");
+				
+				if(scanner.nextInt() == 1) {
+					String sql = "INSERT INTO member (name, sex, old, enter) VALUES ('" + name + "', '" + sex + "', '" + old + "', '" + today + "');";
+					pstmt.executeUpdate(sql);
+					System.out.println("追加完了");
+				}
+				
+				System.out.print("[情報追加：1　終了：0]　->　");
+				
+			}
+			
+			
+			scanner.close();
 			
 			
 			// 後処理（リソースのクローズ）
 			rs.close();
 			pstmt.close();
 			con.close();
+			
+			System.out.println("終了");
+			
 			
 		} catch ( Exception e) {
 			
