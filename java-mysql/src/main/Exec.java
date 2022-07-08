9bf83983f5d17be1a1e14c3fcdcbad9899c54b55
kabata358 <kabata358@gmail.com>
2022-07-08 21:14:06 +0900

    Update

--- java-mysql/src/main/Exec.java
+++ java-mysql/src/main/Exec.java
@@ -2,10 +2,6 @@ package main;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Scanner;
 
 public class Exec {
@@ -17,16 +13,17 @@ public class Exec {
 			// データベースに接続
 			Connection con = DriverManager.getConnection(
 				"jdbc:mysql://localhost/sample?useSSL=false",
-				"",
-				""
+				"root",
+				"root12345"
 			);
 			
+			
+			/*
 			// SQL文の実行
 			PreparedStatement pstmt = con.prepareStatement("SELECT * FROM member");
 			ResultSet rs = pstmt.executeQuery();
 			
 			// 検索結果を表示
-			/*
 			while (rs.next()) {
 				System.out.println(rs.getString("name"));
 				System.out.println(rs.getString("sex"));
@@ -36,12 +33,27 @@ public class Exec {
 			System.out.println("Connected");
 			
 			
-			
 			Scanner scanner = new Scanner(System.in);
 			
-			System.out.print("[情報追加：1　終了：0]　->　");
+			System.out.print("[追加：1　変更：2　削除：3　全件表示：9　終了：0]　->　");
 			
-			while(scanner.nextInt() == 1) {
+			
+			switch(scanner.nextInt()) {
+			case 1 -> insertData();
+			case 2 -> updateData();
+			case 3 -> deleteData();
+			case 9 -> showData();
+			default -> System.out.println("終了します。");
+			};
+			
+
+			
+			
+			
+			
+			
+			/*
+			while( scanner.nextInt() == 1) {
 				
 				String name, sex;
 				int old;
@@ -73,14 +85,16 @@ public class Exec {
 				System.out.print("[情報追加：1　終了：0]　->　");
 				
 			}
+			*/
+			
 			
 			
 			scanner.close();
 			
 			
 			// 後処理（リソースのクローズ）
-			rs.close();
-			pstmt.close();
+			//rs.close();
+			//pstmt.close();
 			con.close();
 			
 			System.out.println("終了");
@@ -92,6 +106,22 @@ public class Exec {
 			
 		}
 		
+	}
+	
+	public static void insertData() {
+		System.out.println("追加します。");
+	}
+	
+	public static void updateData() {
+		System.out.println("変更します。");
+	}
+	
+	public static  void deleteData() {
+		System.out.println("削除します。");
+	}
+	
+	public static void showData() {
+		System.out.println("全件表示します。");
 	}
 
 }
