bf9b5f46b8f152b56286e339af553b7be68085c9
kabata358 <kabata358@gmail.com>
2022-07-13 21:01:56 +0900

    update

--- java-mysql/src/main/Exec.java
+++ java-mysql/src/main/Exec.java
@@ -13,116 +13,109 @@ import java.util.Scanner;
 public class Exec {
 
 	public static void main(String[] args) {
-		
+
 		try {
+			
+			boolean flag = true;
+			Scanner scanner = new Scanner(System.in);
 			
 			// データベースに接続
 			Connection con = DriverManager.getConnection(
-				"jdbc:mysql://localhost/sample?useSSL=false",
-				"root",
-				"root12345"
-			);
+					"jdbc:mysql://localhost/sample?useSSL=false",
+					"root",
+					"root12345");
 			
-			System.out.println("Connected");
-			
-			Scanner scanner = new Scanner(System.in);
-			
-			System.out.print("[追加：1　変更：2　削除：3　全件表示：9　終了：0]　->　");
-			
-			
-			switch(scanner.nextInt()) {
-			case 1 -> insertData(con);
-			case 2 -> updateData(con);
-			case 3 -> deleteData(con);
-			case 9 -> showData(con);
-			default -> System.out.println("終了します。");
-			};
-			
-			
-			
-			
-			
-			
-			scanner.close();
-			
+			// データベースを操作
+			while (flag) {
+				System.out.print("[追加：1　変更：2　削除：3　全件表示：9　終了：0]　->　");
+				
+				switch (scanner.nextInt()) {
+				case 1 -> insertData(con);
+				case 2 -> updateData(con);
+				case 3 -> deleteData(con);
+				case 9 -> showData(con);
+				default -> flag = false;
+				}
+				
+				System.out.println();
+			}
 			
 			// 後処理（リソースのクローズ）
+			scanner.close();
+			con.close();
 			//rs.close();
 			//pstmt.close();
-			con.close();
 			
-			System.out.println("終了");
-			
-			
-		} catch ( Exception e) {
-			
+		} catch (Exception e) {
+
 			System.out.println(e);
-			
+
 		}
-		
+
 	}
-	
+
 	public static void insertData(Connection con) throws SQLException {
 		
 		String name, sex, today;
 		int old;
+		Scanner scanner = new Scanner(System.in);
 		
 		String sql = "INSERT INTO member (name, sex, old, enter) VALUES (?, ?, ?, ?)";
 		PreparedStatement pstmt = con.prepareStatement(sql);
 		
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("追加する情報を入力してください。");
-		
 		// 情報入力
-		System.out.print("名前：");
+		System.out.println("追加する情報を入力してください。");
+		System.out.print("名前　->　");
 		name = scanner.next();
-		System.out.print("性別：");
+		System.out.print("性別　->　");
 		sex = scanner.next();
-		System.out.print("年齢：");
+		System.out.print("年齢　->　");
 		old = scanner.nextInt();
-
+		
 		// 日付を取得
 		Calendar cl = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		today = sdf.format(cl.getTime());
-		
+
 		// 確認
+		System.out.println();
 		System.out.println("'" + name + ", " + sex + ", " + old + "'" + "　---　このデータを追加しますか？");
 		System.out.print("[追加：1　取り消し：0]　->　");
-		
-		if(scanner.nextInt() == 1) {
+
+		if (scanner.nextInt() == 1) {
 			pstmt.setString(1, name);
 			pstmt.setString(2, sex);
 			pstmt.setInt(3, old);
 			pstmt.setString(4, today);
 			pstmt.executeUpdate();
 			System.out.println("追加完了");
-		}			
-}
-	
+		} else {
+			System.out.println("取り消されました");
+		}
+		
+		// 後処理（リソースのクローズ）
+		pstmt.close();
+	}
+
 	public static void updateData(Connection con) throws SQLException {
 		showData(con);
-		
+
 		int id;
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("更新するidを入力してください。");
-		
+
 		System.out.print("id：");
 		id = scanner.nextInt();
-		
-		
-		
+
 		String name, sex, today;
 		int old;
-		
+
 		String sql = "UPDATE member SET name = ?, sex = ?, old = ?, enter = ? WHERE id =" + id;
 		PreparedStatement pstmt = con.prepareStatement(sql);
-		
-		
+
 		System.out.println("更新する情報を入力してください。");
-		
+
 		// 情報入力
 		System.out.print("名前：");
 		name = scanner.next();
@@ -135,12 +128,12 @@ public class Exec {
 		Calendar cl = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		today = sdf.format(cl.getTime());
-		
+
 		// 確認
 		System.out.println("'" + name + ", " + sex + ", " + old + "'" + "　---　このデータに更新しますか？");
 		System.out.print("[更新：1　取り消し：0]　->　");
-		
-		if(scanner.nextInt() == 1) {
+
+		if (scanner.nextInt() == 1) {
 			pstmt.setString(1, name);
 			pstmt.setString(2, sex);
 			pstmt.setInt(3, old);
@@ -149,40 +142,39 @@ public class Exec {
 			System.out.println("更新完了");
 		}
 	}
-	
-	public static  void deleteData(Connection con) throws SQLException {
-		
+
+	public static void deleteData(Connection con) throws SQLException {
+
 		showData(con);
-		
+
 		int id;
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("削除するidを入力してください。");
-		
+
 		System.out.print("id：");
 		id = scanner.nextInt();
-		
-		
-		
+
 		String sql = "DELETE FROM member WHERE id =" + id;
 		Statement smt = con.createStatement();
 		int rowsCount = smt.executeUpdate(sql);
-		
+
 		System.out.print(rowsCount + "レコード削除しました。");
-		
+
 		smt.close();
-		
+
 	}
-	
+
 	public static void showData(Connection con) throws SQLException {
 		String sql = "SELECT * FROM member";
 		PreparedStatement pstmt = con.prepareStatement(sql);
 		ResultSet rs = pstmt.executeQuery();
-		
+
 		while (rs.next()) {
-			System.out.printf("%d\t%s\t%s\t%d\t%s\t%s\n", rs.getInt("id"), rs.getString("name"), rs.getString("sex"), rs.getInt("old"), rs.getString("enter"), rs.getString("memo"));
+			System.out.printf("%d\t%s\t%s\t%d\t%s\t%s\n", rs.getInt("id"), rs.getString("name"), rs.getString("sex"),
+					rs.getInt("old"), rs.getString("enter"), rs.getString("memo"));
 		}
-		
+
 	}
 
 }
